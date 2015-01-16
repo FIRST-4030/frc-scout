0fc75fb5213a008d94dfd54819b6fe4448e14b56
Sam Wolfson <terabyte128@gmail.com>
2015-01-15 20:14:11 -0800

    update gitignore

--- venv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/Sam/PycharmGithub/frc_scout_2015/venv/bin/python
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
--- venv/lib/python3.4/site-packages/compressor/__init__.py
+++
@@ -1,2 +0,0 @@
-# following PEP 386
-__version__ = "1.4"
