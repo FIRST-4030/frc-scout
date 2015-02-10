66be8e9b44f4313027a77b6e7d0a2a5d2d2c0a4b
Sam Wolfson <terabyte128@gmail.com>
2015-02-09 21:21:42 -0800

    improved scout messaging

--- frc_scout/models.py
+++ frc_scout/models.py
@@ -31,7 +31,7 @@ class UserProfile(models.Model):
     team_manager = models.BooleanField(default=False)
     approved = models.BooleanField(default=False)
 
-    position = models.TextField(null=True)
+    message = models.TextField(null=True)
 
     def __str__(self):
         return self.user.username + " - " + self.user.get_full_name() + " - " + str(self.team.team_number)
