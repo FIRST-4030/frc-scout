1e6fe238cee1155da2757b0defa803f847b72888
Sam Wolfson <terabyte128@gmail.com>
2015-03-02 21:32:46 -0800

    fixed a giant security hole where anyone could edit any team's matches

--- frc_scout/views/team_management_views.py
+++ frc_scout/views/team_management_views.py
@@ -171,6 +171,10 @@ def edit_match_post(request):
 
         try:
             match = Match.objects.get(id=pk)
+
+            if match.scout.userprofile.team.team_number != request.user.userprofile.team.team_number:
+                return HttpResponse(status=403)
+
         except Match.DoesNotExist:
             return HttpResponse(status=400)
 
