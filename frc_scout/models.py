08578fb21bd618ca5bb22852d1e8c683d2c6d159
Benjamin <fellfromatree@gmail.com>
2016-01-12 16:13:43 +0000

    Removed google analytics

--- frc_scout/models.py
+++ frc_scout/models.py
@@ -1,8 +1,8 @@
 from django.contrib.auth.models import User
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
-from django.utils import timezone
 
 User._meta.get_field('email')._unique = True
 
